e37ffedaba5ae0cd5ba1e2cd8e5173700729c4b6
visitexoworld <benjamin1995202022@gmail.com>
2022-09-24 21:32:21 -0700

    secc

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb://localhost:27017/mydb', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
-const session = require('express-session')
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
-const MongoDBStore = require('connect-mongodb-session')(session) // add this package to store the user session id automatically on mongodb
 // check on your db, you will have another collection (next to people) which is 'mySessions'
 const loginRouter = require('./routes/loginRoutes')
+const connectDB = require('./config/db')
 
 const app = express()
-const MAX_AGE = 1000 * 60 * 60 * 3 // 3hrs
 const port = process.env.PORT || 5001
 
 const corsOptions = {
@@ -16,31 +13,7 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 // This is where your API is making its initial connection to the database
-mongoose.Promise = global.Promise
-// mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
-//   useNewUrlParser: true,
-// })
-mongoose.createConnection('mongodb://localhost:27017/mydb').name; // "mydb"
-// setting up connect-mongodb-session store
-const mongoDBstore = new MongoDBStore({
-  uri: process.env.DATABASE_CONNECTION_STRING,
-  collection: 'mySessions',
-})
-
-app.use(
-  session({
-    secret: 'a1s2d3f4g5h6',
-    name: 'session-id', // cookies name to be put in "key" field in postman
-    store: mongoDBstore,
-    cookie: {
-      maxAge: MAX_AGE, // this is when our cookies will expired and the session will not be valid anymore (user will be log out)
-      sameSite: false,
-      secure: false, // to turn on just in production
-    },
-    resave: true,
-    saveUninitialized: false,
-  })
-)
+connectDB()
 
 app.use(cors(corsOptions))
 app.use(express.json())
